ed1a4a30ee93c4ff157002eb0b9ac1636710b0f2
RexShack <rexshackuk@gmail.com>
2022-10-11 01:00:11 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'RSG : Cayo Perico Island MiniMap'
+
+data_file "SCALEFORM_DLC_FILE" "stream/int3232302352.gfx"
+
+files {
+  "stream/int3232302352.gfx"
+}
